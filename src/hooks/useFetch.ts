ccd3a512a4e5dd9be4f7258376fe1d4e5fbe2196
ALA YOUNES <ala.younes@eviden.com>
2023-12-13 11:36:01 +0100

    0003 - UPDATE - refactor

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,18 +3,34 @@ import { useCallback, useEffect, useState } from "react";
 type Props<T> = {
   apiVariant: string;
   initialValue: T;
-  queryTerm?: string;
+  queryTerm?: string | undefined;
 };
+
+// type BuildUrlProps<T> = Omit<Props<T>, "initialValue">;
+type BuildUrlProps<T> = Pick<Props<T>, "apiVariant" | "queryTerm">;
+
 // TODO a lib or a hook to check types of env vars
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const VITE_TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
+function buildUrl<T>({ apiVariant, queryTerm }: BuildUrlProps<T>) {
+  const baseEndpoint = `${BASE_URL}/${
+    queryTerm ? "search" : "movie"
+  }${apiVariant}`;
+
+  const queryString = `api_key=${VITE_TMDB_API_KEY}${
+    queryTerm ? `&query=${queryTerm}` : ""
+  }`;
+  return `${baseEndpoint}?${queryString}`;
+}
+
 function useFetch<T>({ apiVariant, initialValue, queryTerm }: Props<T>) {
   const [data, setData] = useState<T>(initialValue);
 
-  const url = queryTerm
-    ? `${BASE_URL}/search${apiVariant}?api_key=${VITE_TMDB_API_KEY}&query=${queryTerm}`
-    : `${BASE_URL}/movie${apiVariant}?api_key=${VITE_TMDB_API_KEY}`;
+  const url = buildUrl({
+    apiVariant,
+    queryTerm,
+  });
 
   const fetchData = useCallback(async () => {
     try {
